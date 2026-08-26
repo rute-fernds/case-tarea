f5eb7fec1ec777c7cc269cf9f9ec664e1759e6f4
rute-fernds <rute.a.fernds@gmail.com>
2026-08-25 23:53:43 -0300

    refactor(config): Adiciona as variáveis referentes ao caminho dos arquivos json, caminho do banco e também as configurações do tesseract

--- src/config.py
+++ src/config.py
@@ -1,13 +1,14 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CAMINHO_PDFS = os.path.join(BASE_DIR, 'data', 'raw')
 
+CAMINHO_PDFS = os.path.join(BASE_DIR, 'data', 'raw')
 CAMINHO_JSON_EXTRAIDO = os.path.join(BASE_DIR, 'data', 'processed', 'texto_extraido.json')
 CAMINHO_JSON_CLASSIFICADO = os.path.join(BASE_DIR, 'data', 'processed', 'texto_classificado.json')
+CAMINHO_DB = os.path.join(BASE_DIR, 'lancedb')
 
-# Configurações do Tesseract
 CAMINHO_TESSERACT = r'C:\Program Files\Tesseract-OCR'
+
 CONFIG_TESSERACT = r'-l por --psm 6'
 
 NUM_THREADS = 6
