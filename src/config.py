99851f2fb9b6feb6f15f352db75599c2a732f73f
rute-fernds <rute.a.fernds@gmail.com>
2026-08-27 01:13:42 -0300

    refactor(config): Adiciona variáveis de caminho de imagens para interface web do sistema

--- src/config.py
+++ src/config.py
@@ -7,8 +7,13 @@ CAMINHO_JSON_EXTRAIDO = os.path.join(BASE_DIR, 'data', 'processed', 'texto_extra
 CAMINHO_JSON_CLASSIFICADO = os.path.join(BASE_DIR, 'data', 'processed', 'texto_classificado.json')
 CAMINHO_DB = os.path.join(BASE_DIR, 'lancedb')
 
+ICONE_USUARIO = os.path.join(BASE_DIR, "src", "img", "user.png")
+ICONE_MODELO = os.path.join(BASE_DIR, "src", "img", "ia.png")
+FAVICON = os.path.join(BASE_DIR, "src", "img", "icone.png")
+
 CAMINHO_TESSERACT = r'C:\Program Files\Tesseract-OCR'
 
 CONFIG_TESSERACT = r'-l por --psm 6'
 
 NUM_THREADS = 6
+
